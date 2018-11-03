d6dac10fb44028acf95167f5caafdabaf5db1d9b
sudarshinityagi <tyagi.sudarshini@gmail.com>
2018-11-03 13:24:49 -0400

    Added server and client

---
+++ run_game.py
@@ -0,0 +1,33 @@
+import sys
+
+from argparse import ArgumentParser
+from multiprocessing import Process
+from time import sleep
+
+from dating_server import GameServer
+
+from clients.matchmaker_client import MatchMaker
+from clients.player_client import Player
+
+
+def init_matchmaker(name):
+    sleep(1)
+    player = MatchMaker(name=name)
+    player.play_game()
+
+def init_player(name):
+    sleep(1)
+    player = Player(name=name)
+    player.play_game()
+
+def main():
+
+    player_1 = Process(target=init_matchmaker, args=('Player Sam',))
+    player_1.start()
+    player_2 = Process(target=init_player, args=('Matchmaker Inav',))
+    player_2.start()
+
+    controller = GameServer()
+
+if __name__ == '__main__':
+    main()
