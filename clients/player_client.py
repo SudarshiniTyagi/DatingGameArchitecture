b4fadb84b4281f0cd00ff64cfd9d932108ab51cc
samcoolidge <sam.r.coolidge@gmail.com>
2018-11-05 16:13:32 -0500

    Update player_client.py

--- clients/player_client.py
+++ clients/player_client.py
@@ -1,5 +1,5 @@
 import json
-from random import randint
+from random import randint,uniform
 
 from clients.client import Player
 
@@ -10,25 +10,29 @@ class Player(Player):
         game_info = json.loads(self.client.receive_data())
         print('Player', game_info)
         self.n = game_info['n']
-        self.initial_weights = [None]*self.n
-        self.current_weights = [None]*self.n
+        self.weight_history = []
+        self.time_left = 120
+        self.candidate_history = []
 
     def play_game(self):
         response = {}
         while True:
-            new_weights = self.your_algorithm(response['candidate_history'])
-            self.client.send_data(json.dumps({"new_weights": new_weights}))
+            new_weights = self.your_algorithm(0 if not response else self.candidate_history)
+            self.client.send_data(json.dumps(new_weights))
             self.current_weights = new_weights
-            response = json.loads(self.client.receive_data())
-            print("New Candidate Received")
+            response = json.loads(self.client.receive_data(size=16184))
             if 'game_over' in response:
-                if 'match_found' in response:
+                if response['match_found']:
                     print("Perfect Candidate Found")
-                    print("Total candidates used = ", response['final_score'])
+                    print("Total candidates used = ", response['num_iterations'])
                 else:
                     print("Perfect candidate not found")
                     print("Total candidates used = ", response['final_score'])
                 exit(0)
+            else:
+                self.time_left = response['time_left']
+                self.candidate_history.append(response['new_candidate'])
+                self.weight_history = response['weight_history']
 
 
 
@@ -36,9 +40,17 @@ class Player(Player):
         """
         PLACE YOUR ALGORITHM HERE
         As the player, you have access to the number of attributes (self.n),
-        the history of candidates (self.candidate_history) and clock time left.
+        the history of candidates (candidate_history)
+        and clock time left (self.time_left).
         You must return an array of weights.
         The weights may be positive or negative ranging from -1 to 1 and
         specified as decimal values having at most two digits to the right of the decimal point, e.g. 0.13 but not 0.134
+        Also the sum of negative weights must be -1 and the sum of positive weights must be 1.
         """
-        return randint(-1, 1)
+
+        #print(self.n)
+        #print(candidate_history)
+        #print(self.weight_history)
+        #print(self.time_left)
+
+        return [1,-1] + [0 for i in range(self.n - 2)]
