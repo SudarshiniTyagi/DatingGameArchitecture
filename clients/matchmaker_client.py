d179fa7a19023d0fc6d27ae72e77975ac8b9af3e
samcoolidge <sam.r.coolidge@gmail.com>
2018-11-05 16:13:47 -0500

    Update matchmaker_client.py

--- clients/matchmaker_client.py
+++ clients/matchmaker_client.py
@@ -1,42 +1,54 @@
 import json
+from random import random
 
 from clients.client import Player
 
 class MatchMaker(Player):
     def __init__(self, name):
         super(MatchMaker, self).__init__(name=name, is_player=False)
-        game_info = json.loads(self.client.receive_data())
+        game_info = json.loads(self.client.receive_data(size=16184))
         print('Matchmaker', game_info)
-        self.random_candidates_and_scores = game_info['random_candidates_and_scores']
+        self.random_candidates_and_scores = game_info['randomCandidateAndScores']
         self.n = game_info['n']
-        self.candidate_score = {'candidate': [], 'score': 0}
+        self.prev_candidate = {'candidate': [], 'score': 0, 'iter':0}
+        self.time_left = 120
 
     def play_game(self):
-        first_candidate = self.my_candidate()
-        self.client.send_data(json.dumps({"first_candidate": first_candidate}))
+
         while True:
             candidate = self.my_candidate()
-            self.client.send_data(json.dumps({"candidate": candidate}))
+            self.client.send_data(json.dumps(candidate))
             response = json.loads(self.client.receive_data())
-            self.candidate_score = response['candidate_score']
-            print("Candidate Score: ", self.candidate_score)
-
             if 'game_over' in response:
-                if 'match_found' in response:
+                if response['match_found']:
                     print("Perfect Candidate Found")
-                    print("Total candidates used = ", response['final_score'])
+                    print("Total candidates used = ", response['num_iterations'])
                 else:
                     print("Perfect candidate not found")
                     print("Total candidates used = ", response['final_score'])
                 exit(0)
+            else:
+                self.prev_candidate = response['prev_candidate']
+                self.time_left = response['time_left']
 
     def my_candidate(self):
+
         """
         PLACE YOUR CANDIDATE GENERATION ALGORITHM HERE
         As the matchmaker, you have access to the number of attributes (self.n),
         initial random candidates and their scores (self.random_candidates_and_scores),
-        score of each candidate (self.candidate_score) and clock time left.
+        your clock time left (self.time_left)
+        and a dictionary of the previous candidate sent (self.prev_candidate) consisting of
+            'candidate' = previous candidate attributes
+            'score' = previous candidate score
+            'iter' = iteration num of previous candidate
         For this function, you must return an array of values that lie between 0 and 1 inclusive and must have four or
         fewer digits of precision. The length of the array should be equal to the number of attributes (self.n)
         """
-        return []
+
+        #print(self.n)
+        #print(self.random_candidates_and_scores)
+        #print(self.time_left)
+        #print(self.prev_candidate)
+
+        return [round(random(),4) for i in range(self.n)]
